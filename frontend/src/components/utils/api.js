fdba6b09361811024bf2d6b9105bb70b1f2f881c
Lemon House <2804622326@qq.com>
2025-08-21 10:21:08 +0100

    Update api.js

--- frontend/src/components/utils/api.js
+++ frontend/src/components/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Determine the base URL for API requests. The value can be overridden
 // using the `VITE_API_BASE_URL` environment variable; otherwise, it falls
 // back to the local development server.
-const baseURL = process.env.VITE_API_BASE_URL || "http://localhost:9192/api/v1";
+const baseURL = process.env.VITE_API_BASE_URL || "http://54.228.78.4:9192/api/v1";
 
 export const api = axios.create({
   baseURL,
